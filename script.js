13b820755cfafe3a2f49a0c4efee7b41942de791
Your Namef <obiobialo@aol.com>
2022-09-01 00:21:08 -0500

    redesigned website and added newsletter subscription

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 AOS.init({
-    duration: 1000,
-})
+  duration: 1000,
+});
+
+const form = document.getElementById("sheetdb-form");
+const input = document.getElementById("input");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  }).then((response) => response.json(), (input.value = ""));
+});
